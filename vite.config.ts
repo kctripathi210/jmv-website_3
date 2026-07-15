3418efc17c8b80838405bd679c54e6ecfb9583b3
kctripathi210 <kctripathi210@gmail.com>
2026-07-15 11:53:47 +0530

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/jmv-website_3/',
+  base: './',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  // Yeh block aapki missing assets ki wajah se white screen crash ko rokega
-  resolve: {
-    alias: {
-      '/asset': '/public/asset',
-      '/assets': '/public/asset'
-    }
-  }
 });
